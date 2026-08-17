0e48b0da5e5f0c2ba710fdbd5ae0b5f71c8f9ef3
rajarmxx1-max <rajarmxx1@gmail.com>
2026-08-17 10:43:46 +0800

    Create runtime-config.example.js

---
+++ runtime-config.example.js
@@ -0,0 +1,14 @@
+/* Firebase configuration for pikjamail */
+const firebaseConfig = {
+  apiKey: "AIzaSyB9XOFZmVn5AoiLOPA1E3dqa8s4yraki58",
+  authDomain: "pikjamail.firebaseapp.com",
+  databaseURL: "https://pikjamail-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "pikjamail",
+  storageBucket: "pikjamail.firebasestorage.app",
+  messagingSenderId: "839359291424",
+  appId: "1:839359291424:web:a588a2121104e6fc945c45",
+  measurementId: "G-97X9NDSJVW"
+};
+
+window.RUNTIME_CONFIG = window.RUNTIME_CONFIG || {};
+window.RUNTIME_CONFIG.firebase = firebaseConfig;
