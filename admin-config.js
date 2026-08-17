87804f269ed47eb33ea270eea7c52b07dec261c9
rajarmxx1-max <rajarmxx1@gmail.com>
2026-08-17 10:38:01 +0800

    Create admin-config.js

---
+++ admin-config.js
@@ -0,0 +1,7 @@
+/* Admin identity configuration.
+   UID is an identifier, not a password or secret.
+*/
+window.ADMIN_CONFIG = {
+  uid: "hUUBbw8j3JViM7ZBXJ52UWFp7go2",
+  username: "paneladmin"
+};
